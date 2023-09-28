82dd7c9d16445b7ae682f5c0fdd4414fc5d783c4
Javier Trias <jtrias3000@gmail.com>
2023-09-28 15:39:43 -0400

    New page added with modified index page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>I'm a software engineer/developer that is aiming to break the ice through the ever-dynamic tech industry!</p>
+    </main>
+  )
+}
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
