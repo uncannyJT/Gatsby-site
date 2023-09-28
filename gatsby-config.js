8427dac2de7c80368fccee11a34774fb3be5f749
Javier Trias <jtrias3000@gmail.com>
2023-09-28 16:18:42 -0400

    Add gatsby-plugin-netlify to the plugin array

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Javier's Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
