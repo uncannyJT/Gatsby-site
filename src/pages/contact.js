44ee9f951a7617400f8dc18742c5dce36407d36a
Javier Trias <jtrias3000@gmail.com>
2023-10-23 16:24:34 -0400

    first contact page that returns the new Link component

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from 'gatsby'
+import Layout  from "../components/layout"
+
+
+const ContactPage = () => {
+  return (
+    <Layout></Layout>
+  )
+}
+
+export default ContactPage
