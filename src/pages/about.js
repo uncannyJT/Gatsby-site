4cad1624a745c60aad4dc48911c52e715dce174b
Javier Trias <jtrias3000@gmail.com>
2023-10-23 15:41:15 -0400

    made new hyperlink to return to home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
@@ -6,6 +7,7 @@ const AboutPage = () => {
       <h1>About Me</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
       <p>I'm a software engineer/developer that is aiming to break the ice through the ever-dynamic tech industry!</p>
+      <Link to="/">👉 Back to Home Page</Link>
     </main>
   )
 }
