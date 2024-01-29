dab0ff668dd2a3ee98dd54645c72e1324d8166ea
Javier Trias <jtrias3000@gmail.com>
2024-01-29 14:39:11 -0500

    Added a configuration object for the blog dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,17 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", {
+    // resolve: 'gatsby-source-filesystem',
+    // options: {
+    //   "name": "images",
+    //   "path": `./src/images`
+    // },
+    // __key: "images",
+
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      "name": "blog",
+      "path": `${__dirname}/blog`
     },
-    __key: "images"
   }]
 };
