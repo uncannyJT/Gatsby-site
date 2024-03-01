ce7edbc63ae0458825c0ac8d562805e73f4915fc
Javier Trias <jtrias3000@gmail.com>
2024-03-01 15:32:23 -0500

    Installed 'gatsby-plugin-mdx' transformer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `JT's Porfolio Website`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-netlify", "gatsby-plugin-mdx", {
     // resolve: 'gatsby-source-filesystem',
     // options: {
     //   "name": "images",
