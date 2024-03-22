d8081474d0f1c4ef629bc900e8679210a3804c75
Javier Trias <jtrias3000@gmail.com>
2024-03-22 16:10:39 -0400

    Created new file for blog post page template

---
+++ src/pages/{mdx.frontmatter__slug}.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPost = () => {
+  return (
+    <Layout pageHeading="Cool Blog Posts">
+      <p>My blog post contents will go here (some time soon).</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <Seo title="Some Quality Blog Posts" />
+
+export default BlogPost
